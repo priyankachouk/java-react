c67e8285d5b1e45e31fb220e75e75084adb928ac
root <root@ip-172-31-86-106.ec2.internal>
2022-10-28 08:55:31 +0000

    added data

---
+++ Jenkinsfile
@@ -0,0 +1,25 @@
+pipeline {
+  agent any
+	
+      stages {
+	     stage('AMI creation') {  
+	  steps {
+          sh 'packer --version'
+		  sh 'ls'
+		  sh 'sudo packer build -var-file=./packer/vars.json ./packer/packer.json'
+          }
+       }
+	       stage('terraform') {  
+	  steps {
+         
+		  withAWS(credentials: 'aws-cred') {
+		 
+		   sh 'ls'
+		  sh 'terraform init'
+		  sh 'terraform apply -auto-approve'
+		}
+		 
+          }
+       }
+      }
+ }
---
+++ main.tf
@@ -0,0 +1,49 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["packer-mysql-sa"]
+  }
+}
+
+data "aws_eip" "my_instance_eip" {
+  public_ip = "3.220.181.223"
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.ec2_instance.id
+  allocation_id = data.aws_eip.my_instance_eip.id
+}
+
+resource "aws_instance" "ec2_instance" {
+  ami                    = data.aws_ami.ubuntu.id
+  key_name               = "packer_key"
+  monitoring             = true
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = ["sg-0831e267a4376280c"]
+
+  user_data = "${file("user_data123.sh")}"
+
+  tags = {
+    Name = "terraform-app"
+  }
+ 
+}
+
